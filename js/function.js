c598ebc698bda1912312a3a04d9edaa00261bafb
HassanDurra <109581409+HassanDurra@users.noreply.github.com>
2023-01-23 20:21:40 +0500

    portfolio

---
+++ js/function.js
@@ -0,0 +1,139 @@
+
+function Showsearch(){
+
+    var modal = document.getElementById('searcharea');
+    modal.style.display='block';
+ 
+
+
+
+
+}
+    function Showsearch1(){
+        document.getElementById('searcharea').style.display='none';
+    }
+    // about function
+   function Page(){
+       document.getElementById('About').style.display="Block";
+        
+   } 
+//    about close
+   function HideAbout(){
+       document.getElementById('About').style.display="none";
+   }
+ 
+// Cv Resume section 
+// Project section start
+
+   function Projects(){
+       document.getElementById('Projects').style.display="block";
+   }
+
+// Project section close
+   function HideProgrammes(){
+       document.getElementById('Projects').style.display = "none";
+   }
+   
+   
+// show hidden div
+   function ShowDiv(){
+  document.getElementById('hidden').style.display="block";
+   }
+
+    // hide div 
+    function hideDiv(){
+        document.getElementById('hidden').style.display="none";
+    }   
+    // hide div 2
+function ShowDiv1(){
+    document.getElementById('hidden1').style.display="block";
+     }
+  
+     // hide div 2
+         function hideDiv1(){
+          document.getElementById('hidden1').style.display="none";
+      }    
+
+    // hide div 3
+function ShowDiv2(){
+    document.getElementById('hidden2').style.display="block";
+     }
+  
+  // hide div 3
+      function hideDiv2(){
+          document.getElementById('hidden2').style.display="none";
+      }   
+
+    // hide div 4
+function ShowDiv3(){
+    document.getElementById('hidden3').style.display="block";
+     }
+  
+  // hide div 4
+      function hideDiv3(){
+          document.getElementById('hidden3').style.display="none";
+      }            
+
+      
+    // hide div 5
+function ShowDiv4(){
+    document.getElementById('hidden4').style.display="block";
+     }
+  
+  // hide div 5
+      function hideDiv4(){
+          document.getElementById('hidden4').style.display="none";
+      }     
+      
+  
+    // hide div 6
+function ShowDiv5(){
+    document.getElementById('hidden5').style.display="block";
+     }
+  
+  // hide div 6
+      function hideDiv5(){
+          document.getElementById('hidden5').style.display="none";
+      }            
+
+          // hide div 7
+function ShowDiv6(){
+    document.getElementById('hidden6').style.display="block";
+     }
+  
+  // hide div 6
+      function hideDiv6(){
+          document.getElementById('hidden6').style.display="none";
+      }      
+      
+          // hide div 7
+function ShowDiv7(){
+    document.getElementById('hidden7').style.display="block";
+     }
+  
+  // hide div 6
+      function hideDiv7(){
+          document.getElementById('hidden7').style.display="none";
+      }    
+// text animation
+// show resume
+function showresume(){
+    document.getElementById('Resume').style.display="Block";
+}
+
+// hide resume
+
+
+function hideResume(){
+    document.getElementById('Resume').style.display="none";
+}
+// contact section
+// show contact
+function showContact(){
+document.getElementById('Contact').style.display="block";
+}
+
+// hide contact
+function hideContact(){
+    document.getElementById('Contact').style.display="none";
+}
